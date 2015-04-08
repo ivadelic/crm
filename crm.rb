ab6f884a2eed07bedb1b007f525002c369231690
Iva Delic <ms.iva.delic@gmail.com>
2015-04-08 15:49:22 -0400

    Created new-contact route

--- crm.rb
+++ crm.rb
@@ -1,17 +1,20 @@
 require_relative 'contacts'
 require 'sinatra'
 
-
-
 get '/' do 
 	"Main Menu"
 	erb :index
 end
 
-get '/contacts' do
-	erb :contacts 
+get "/contacts" do 
+	@contacts = [ ]
+	@contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
+	@contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
+	@contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
+
+	erb :contacts
 end
 
 get '/contacts/new' do
-	erb :new_contacts  
+	erb :new 
 end
