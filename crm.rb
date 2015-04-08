efd014a80a741ac3e06e8f63089c1ccb2ff46fa1
Iva Delic <ms.iva.delic@gmail.com>
2015-04-08 16:54:11 -0400

    Finished assignment.. but will do it again

--- crm.rb
+++ crm.rb
@@ -1,20 +1,24 @@
 require_relative 'contacts'
+require_relative 'rolodex'
 require 'sinatra'
 
+$rolodex= Rolodex.new
+
 get '/' do 
-	"Main Menu"
+	@crm_app_name = "Iva's EVEN COOLER CRM"
 	erb :index
 end
 
 get "/contacts" do 
-	@contacts = [ ]
-	@contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
-	@contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-	@contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
-
 	erb :contacts
 end
 
-get '/contacts/new' do
-	erb :new 
+get '/contacts/new' do 
+	erb :new_contact
 end
+
+post '/contacts' do
+	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email])
+	$rolodex.add_contact(new_contact)
+	redirect to ('/contacts')
+end 
