69648fa73465b8c45512c55c7ccb64aabf288ad3
Iva Delic <ms.iva.delic@gmail.com>
2015-04-08 15:08:33 -0400

    Added contacts

--- crm.rb
+++ crm.rb
@@ -1,6 +1,17 @@
+require_relative 'contacts'
 require 'sinatra'
+
+
 
 get '/' do 
 	"Main Menu"
 	erb :index
+end
+
+get '/contacts' do
+	erb :contacts 
+end
+
+get '/contacts/new' do
+	erb :new_contacts  
 end
